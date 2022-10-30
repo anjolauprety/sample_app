427a134edecb5e193cad62697f5a186253a3be92
Anjola Uprety <anjolauprety@brandeis.edu>
2022-10-29 22:51:27 -0400

    Finish user edit, update,index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -10,7 +10,9 @@ gem "bootstrap-sass",  "3.4.1"
 gem 'sassc-rails', '2.1.2'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
-
+gem "faker"
+gem "will_paginate",           "3.3.1"
+gem "bootstrap-will_paginate", "1.0.0"
 # Use sqlite3 as the database for Active Record
 # gem "sqlite3", "~> 1.4"
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
-class SessionsController < ApplicationController
+require_relative '../helpers/sessions_helper'
 
-  def new
-  end
+class SessionsController < ApplicationController
+  include(SessionsHelper)
+
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session
       log_in user
-      redirect_to user
+      redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -17,6 +20,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root_path, status: :see_other
+    redirect_to root_url, status: :see_other
   end
 end
